d23a9b94a9e4b7037279722f6534469f426aec0f
louis-udm <louis.lv.info@gmail.com>
2022-07-23 18:05:41 -0400

    improve 02

--- 1-100q/02.py
+++ 1-100q/02.py
@@ -3,6 +3,7 @@
 其中，它们各自的位数是按照 **逆序** 的方式存储的，并且它们的每个节点只能存储 **一位** 数字。
 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。您可以假设除了数字 0 之外，
 这两个数都不会以 0 开头。
+https://zhenyu0519.github.io/2020/06/06/lc02/
 """
 
 from __future__ import annotations
@@ -28,6 +29,18 @@ class ListNode:
                 pre = node
         return head
 
+    def copy(self):
+        head: Optional[ListNode] = None
+        if self:
+            p = self
+            head = ListNode(p.val)
+            cur = head
+            while p.next:
+                p = p.next
+                cur.next = ListNode(p.val)
+                cur = cur.next
+        return head
+
     def __str__(self):
         p = self
         s = ""
@@ -38,25 +51,27 @@ class ListNode:
 
 
 def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
-    result = head = ListNode("inf")
+    head = cur = ListNode("inf")
     carry = 0
     while l1 and l2:
-        carry, cur = divmod(l1.val + l2.val + carry, 10)
-        node = ListNode(cur)
-        head.next = node
-        head, l1, l2 = head.next, l1.next, l2.next
-    head.next = l1 if l1 else l2
-    while carry and head.next:
-        carry, cur = divmod(head.next.val + carry, 10)
-        head.next.val = cur
-        head = head.next
+        carry, val = divmod(l1.val + l2.val + carry, 10)
+        node = ListNode(val)
+        cur.next = node
+        cur, l1, l2 = cur.next, l1.next, l2.next
+    cur.next = l1.copy() if l1 else l2.copy()
+    while carry and cur.next:
+        carry, val = divmod(cur.next.val + carry, 10)
+        cur.next.val = val
+        cur = cur.next
     if carry:
-        head.next = ListNode(carry)
-    return result.next
+        cur.next = ListNode(carry)
+    return head.next
 
 
-a = ListNode.create_from_list([2, 4, 3])
+a = ListNode.create_from_list([2, 4, 8, 9])
 print(a)
 b = ListNode.create_from_list([5, 6, 4])
 print(b)
 print(addTwoNumbers(a, b))
+print(a)
+print(b)
