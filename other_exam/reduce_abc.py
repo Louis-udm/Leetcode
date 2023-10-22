9fbd6d9411a8ee355e2b9d761c3a3be46a1fbe96
Zhibin Lu <zlu@macbook-zlu.local>
2023-10-22 18:03:13 -0400

    add title for reduce_abc

--- other_exam/reduce_abc.py
+++ other_exam/reduce_abc.py
@@ -1,3 +1,29 @@
+# Have the function StringChallenge(str) take the str parameter being passed and
+# return the smallest number you can get through the following reduction method.
+# The method is: Only the letters a, b, and c will be given in str and you must
+# take two different adjacent characters and replace it with the third. For example
+# "ac" can be replaced with "b" but "aa" cannot be replaced with anything. This
+# method is done repeatedly until the string cannot be further reduced, and the
+# length of the resulting string is to be outputted. For example: if str is "cab",
+# "ca" can be reduced to "b" and you get "bb" (you can also reduce it to "cc").
+# The reduction is done so the output should be 2. If str is "bcab", "bc" reduces
+# to "a", so you have "aab", then "ab" reduces to "c", and the final string "ac"
+# is reduced to "b" so the output should be 1.
+#
+# Examples
+# Input: "abcabc"
+# Output: 2
+# Input: "cccc"
+# Output: 4
+# Input: "cccb"
+# Output: 1
+# Input: "cab"
+# Output: 2
+# Input: "bcab"
+# Output: 1
+# Input: "aabbbc"
+# Output: 1
+
 def reduce_abc(strParam):
   # code goes here
   def one_loop(st, init_i):
@@ -25,3 +51,6 @@ def reduce_abc(strParam):
 
 
 print(reduce_abc("abcabc"))
+print(reduce_abc("cccc"))
+print(reduce_abc("cccb"))
+print(reduce_abc("bcab"))
